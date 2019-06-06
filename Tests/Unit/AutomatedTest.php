ad0276a7388e8b4f0c3c968b27b9abe82d9c7f71
Benjamin Serfhos <benjamin@serfhos.com>
2019-06-06 12:45:39 +0200

    [WIP][TASK] Add tryout for automated testing

---
+++ Tests/Unit/AutomatedTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KoninklijkeCollective\MyUserManagement\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AutomatedTest extends TestCase
+{
+    public function test_automated()
+    {
+        $this->assertTrue(true);
+    }
+}
